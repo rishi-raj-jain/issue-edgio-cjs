18ec9817d204a26fa07fa619adbe64482176f03a
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-23 15:08:16 +0530

    Custom Connector Fails with type: module

---
+++ layer0.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  // Overrides the default path to the routes file. The path should be relative to the root of your app.
+  routes: './routes.cjs',
+  // Custom Connector
+  connector: './layer0'
+};
---
+++ routes.cjs
@@ -0,0 +1,3 @@
+import { Router } from '@layer0/core/router'
+
+export default new Router().static('assets')
